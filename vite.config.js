b6aa5ae87f649cec249357ad9ee958cbdf223069
Wasim <princewasim971@gmail.com>
2024-12-17 07:24:42 -0800

    remove dependance

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,13 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   // server:{
-//   //   port:80,
-//   // },
-//   plugins: [react()],
-// })
-
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    Checker({
-      typescript: false,  // Disable TypeScript checking if you're not using it
-      eslint: false,  // Disable ESLint checking
-      caseSensitivePaths: true, // Enforce case-sensitive path checks
-    }),
-  ],
+  // server:{
+  //   port:80,
+  // },
+  plugins: [react()],
 })
+
+
+
